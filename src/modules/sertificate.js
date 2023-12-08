942400614db426ceef3a9466600d6a23eff9258b
totsky.ia <valihmanov777@gmail.com>
2023-12-08 03:57:31 +0300

    Раскидался

--- src/modules/sertificate.js
+++ src/modules/sertificate.js
@@ -1,8 +1,40 @@
 const sertificate = () => {
   document.addEventListener("DOMContentLoaded", function () {
-    console.log(123);
+    const certificateImages = document.querySelectorAll(
+      ".sertificate-document"
+    );
 
-    //Не получается ничего(((((
+    certificateImages.forEach(function (image) {
+      image.addEventListener("click", function (event) {
+        event.preventDefault();
+        const imageUrl = this.getAttribute("href");
+
+        const modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.style.display = "block";
+        modal.style.top = "-10%";
+        modal.style.left = "25%";
+        modal.innerHTML = `
+          <div class="modal-content">
+            <span class="close">X</span>
+            <img src="${imageUrl}" alt="Certificate">
+          </div>
+        `;
+
+        document.body.appendChild(modal);
+
+        const closeModal = modal.querySelector(".close");
+
+        closeModal.style.position = "relative";
+        closeModal.style.top = "120px";
+        closeModal.style.left = "-415px";
+        closeModal.style.color = "black";
+
+        closeModal.addEventListener("click", function () {
+          modal.remove();
+        });
+      });
+    });
   });
 };
 
